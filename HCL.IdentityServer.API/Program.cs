d7168e5e09bf1bede23f26a5a13862c54fb931bc
Ilia <pops375@yandex.ru>
2023-05-16 22:01:48 +0300

    connect swagger in gcp

--- HCL.IdentityServer.API/Program.cs
+++ HCL.IdentityServer.API/Program.cs
@@ -28,7 +28,7 @@ namespace HCL.IdentityServer.API
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
