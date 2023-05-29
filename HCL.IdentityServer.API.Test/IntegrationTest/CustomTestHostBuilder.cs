c845e5fa270a460bbc7f194473c964a11a3081be
Ilia <pops375@yandex.ru>
2023-05-29 17:04:28 +0300

    fix

--- HCL.IdentityServer.API.Test/IntegrationTest/CustomTestHostBuilder.cs
+++ HCL.IdentityServer.API.Test/IntegrationTest/CustomTestHostBuilder.cs
@@ -11,41 +11,6 @@ namespace HCL.IdentityServer.API.Test.IntegrationTest
 {
     public class CustomTestHostBuilder
     {
-
-        public static WebApplicationFactory<Program> Build(string dbUser, string dbPassword, string dbServer, ushort dbPort, string dbName)
-        {
-            var npgsqlConnectionString = $"User Id={dbUser}; Password={dbPassword}; Server={dbServer}; " +
-                $"Port={dbPort}; Database={dbName}; IntegratedSecurity=true; Pooling=true";
-
-            return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureTestServices(services =>
-                {
-                    var dbContextDescriptor = services.SingleOrDefault(d =>
-                             d.ServiceType == typeof(DbContextOptions<AppDBContext>));
-
-                    services.Remove(dbContextDescriptor);
-
-                    services.AddDbContext<AppDBContext>(options =>
-                    {
-                        options.UseNpgsql(npgsqlConnectionString);
-                    });
-
-                    var grpc = services.SingleOrDefault(d =>
-                             d.ImplementationType == typeof(GrpcEndpoinListenHostService));
-
-                    services.Remove(grpc);
-
-                    var serviceProvider = services.BuildServiceProvider();
-                    using var scope = serviceProvider.CreateScope();
-                    var scopedServices = scope.ServiceProvider;
-                    var context = scopedServices.GetRequiredService<AppDBContext>();
-                    context.Database.EnsureDeleted();
-                    context.Database.Migrate();
-                });
-            });
-        }
-
         public static WebApplicationFactory<Program> BuildWithAdmin(string dbUser, string dbPassword, string dbServer, ushort dbPort, string dbName)
         {
             var npgsqlConnectionString = $"User Id={dbUser}; Password={dbPassword}; Server={dbServer}; " +
@@ -86,45 +51,6 @@ namespace HCL.IdentityServer.API.Test.IntegrationTest
                         Salt = "QTPZeLPujIVXf66A2Ggu0YE4LS2ZC7lZiDd6WGlkpiAyh05WTRmXhRjXZzf90+zrusy65s7ghCnwna6F5A/1swR/xn1VxEgEsrKaVM+vxaST6OwNRjks2Tb6elCAyOEFrplcdGR/JbjxwhO/A1VZqL+259h7CdxvCu6KFc5kZgI=",
                         Password = "5a2CBsDoaFXUG+p+QRrrQlH520CrvNnVIAEEki1Sl0gKiugBx6qyqBMyfriO0+yyg41gIGqofvRn3UZbQn+xyg==",
                     });
-
-                    await context.SaveChangesAsync();
-                });
-            });
-        }
-
-        public static WebApplicationFactory<Program> BuildWithAccounts(string dbUser, string dbPassword, string dbServer
-            , ushort dbPort, string dbName, List<Account> accounts)
-        {
-            var npgsqlConnectionString = $"User Id={dbUser}; Password={dbPassword}; Server={dbServer}; " +
-                $"Port={dbPort}; Database={dbName}; IntegratedSecurity=true; Pooling=true";
-
-            return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
-            {
-                builder.ConfigureTestServices(async services =>
-                {
-                    var dbContextDescriptor = services.SingleOrDefault(d =>
-                             d.ServiceType == typeof(DbContextOptions<AppDBContext>));
-
-                    services.Remove(dbContextDescriptor);
-
-                    services.AddDbContext<AppDBContext>(options =>
-                    {
-                        options.UseNpgsql(npgsqlConnectionString);
-                    });
-
-                    var grpc = services.SingleOrDefault(d =>
-                             d.ImplementationType == typeof(GrpcEndpoinListenHostService));
-
-                    services.Remove(grpc);
-
-                    var serviceProvider = services.BuildServiceProvider();
-                    using var scope = serviceProvider.CreateScope();
-                    var scopedServices = scope.ServiceProvider;
-                    var context = scopedServices.GetRequiredService<AppDBContext>();
-                    context.Database.EnsureDeleted();
-                    context.Database.EnsureCreated();
-
-                    await context.Accounts.AddRangeAsync(accounts);
 
                     await context.SaveChangesAsync();
                 });
